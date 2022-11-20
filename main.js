da52fcea2f08a315d86c719155d22eded4b3df82
Juan Fernando HerreraNoboa <cursos2211@gmail.com>
2022-11-20 18:06:21 -0500

    Aside

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIco = document.querySelector('.menu');
+const menuCarritoIco = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIco.addEventListener('click', toggleMobilepMenu);
+menuCarritoIco.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isMenuCarritoOpen = !aside.classList.contains('inactive');
+    if(isMenuCarritoOpen){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu(){
+    const isMenuCarritoOpen = !aside.classList.contains('inactive');
+    if(isMenuCarritoOpen){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive');
 
 }
 console.log('JS Funciona');
